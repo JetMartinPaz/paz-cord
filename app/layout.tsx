67fabdb575d573c185bdad963569df1657625785
JetMartinPaz <118558403+JetMartinPaz@users.noreply.github.com>
2023-09-10 11:15:57 -0600

    Implementacion de DarkMode

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 import { esES } from '@clerk/localizations'
-
+import { ThemeProvider } from '@/components/providers/theme-provider'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -19,8 +19,17 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider localization={esES}>
-      <html lang="en">
-        <body className={font.className}>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <body className={font.className}>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='dark'
+            enableSystem={false}
+            storageKey='paz-cord-theme'  
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   )
